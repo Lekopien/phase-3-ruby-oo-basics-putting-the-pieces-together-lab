2140e4cba17e4653e75a658011526c43a28a29e7
lekopien <mavericklekopien@gmail.com>
2023-02-14 09:23:45 +0300

    completed assignment-macro to attr_accessor, attr_reader and attr_writer

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
 # Make your shoe class here!
+class Shoe
+    #add attributes
+
+     attr_reader :brand
+     attr_accessor :color, :size, :material, :condition
+
+    #initialize with a brand
+    
+      def initialize(brand)
+        @brand = brand
+     end
+  
+      def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+  
+  end
+  
+  shoe = Shoe.new("Nike")
